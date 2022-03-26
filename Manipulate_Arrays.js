a1e0bc71e8dd45968ac765c3a060e50c1eb4573b
GeroZayas <gerozayas@gmail.com>
2022-03-27 00:19:27 +0100

    update

--- Manipulate_Arrays.js
+++ Manipulate_Arrays.js
@@ -1,22 +1,39 @@
-// Setup
+// PUSH
 const myArray = [
   ["John", 23],
   ["cat", 2],
 ];
 myArray.push(["dog", 3]);
-// Only change code below this line
 
 console.log(myArray);
 
-// Setup
+// POP
 const myArray2 = [
   ["John", 23],
   ["cat", 2],
 ];
 
-// Only change code below this line
 var removedFromMyArray = myArray2.pop();
 
-console.log("********************************");
+console.log(removedFromMyArray);
+
+// SHIFT
+
+const myArray3 = [
+  ["John", 23],
+  ["dog", 3],
+];
+
+var removedFromMyArray = myArray3.shift();
 
 console.log(removedFromMyArray);
+
+// UNSHIFT
+
+const ourArray = ["Stimpson", "J", "cat"];
+console.log(ourArray);
+
+ourArray.shift(); // takes Stimson out
+ourArray.unshift("Happy"); // Puts Happy in first position
+
+console.log(ourArray);

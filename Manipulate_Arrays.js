07fec480601c74fdf09959caba3c14bdf424c4a6
GeroZayas <gerozayas@gmail.com>
2022-03-27 00:13:14 +0100

    Update

---
+++ Manipulate_Arrays.js
@@ -0,0 +1,22 @@
+// Setup
+const myArray = [
+  ["John", 23],
+  ["cat", 2],
+];
+myArray.push(["dog", 3]);
+// Only change code below this line
+
+console.log(myArray);
+
+// Setup
+const myArray2 = [
+  ["John", 23],
+  ["cat", 2],
+];
+
+// Only change code below this line
+var removedFromMyArray = myArray2.pop();
+
+console.log("********************************");
+
+console.log(removedFromMyArray);

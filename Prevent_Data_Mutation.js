eda1ee0cfbe481a16378347f95e99502c132d8df
GeroZayas <gerozayas@gmail.com>
2022-03-29 10:50:43 +0200

    Initial commit

---
+++ Prevent_Data_Mutation.js
@@ -0,0 +1,1 @@
+/*const declaration alone doesn't really protect your data from mutation. To ensure your data doesn't change, JavaScript provides a function Object.freeze to prevent data mutation. */

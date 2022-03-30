f26faa2fbfa647650ab35b03b2ee6143d90af6db
GeroZayas <gerozayas@gmail.com>
2022-03-30 09:11:50 +0200

    Initial Commit

--- Prevent_Object_Mutation.js
+++ Prevent_Object_Mutation.js
@@ -1,1 +1,45 @@
-/*const declaration alone doesn't really protect your data from mutation. To ensure your data doesn't change, JavaScript provides a function Object.freeze to prevent data mutation. */
+/*const declaration alone doesn't really protect your data from mutation. To ensure your data doesn't change, JavaScript provides a function Object.freeze to prevent data mutation. 
+
+Any attempt at changing the object will be rejected, with an error thrown if the script is running in strict mode.
+
+*/
+
+// Example:
+
+let obj = {
+  name: "FreeCodeCamp",
+  review: "Awesome",
+};
+Object.freeze(obj); // we freeze the object to prevent mutation
+obj.review = "bad";
+obj.newProp = "Test";
+console.log(obj);
+
+// console log separator line
+console.log("-------------------------------------------------- 01");
+
+// Exercise
+
+/*In this challenge you are going to use Object.freeze to prevent mathematical constants from changing. You need to freeze the MATH_CONSTANTS object so that no one is able to alter the value of PI, add, or delete properties. */
+
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14,
+  };
+  // Only change code below this line
+  Object.freeze(MATH_CONSTANTS);
+
+  // Only change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch (ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+
+console.log(PI);
+
+// console log separator line
+console.log("-------------------------------------------------- 02");

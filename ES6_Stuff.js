dc8df332580a294f12ef753335554c01f97f646d
GeroZayas <gerozayas@gmail.com>
2022-03-31 10:19:03 +0200

    Update

--- ES6_Stuff.js
+++ ES6_Stuff.js
@@ -9,3 +9,37 @@ function howMany(...args) {
 }
 console.log(howMany(0, 1, 2)); // You have passed 3 arguments.
 console.log(howMany("string", null, [1, 2, 3], {})); // You have passed 4 arguments.
+
+// print separator line
+console.log("-------------------------------------------------- 1");
+
+// Exercise
+const sum = (...args) => {
+  return args.reduce((a, b) => a + b, 0);
+};
+
+console.log(sum(1, 2, 3, 4, 5)); // 15
+
+// print separator line
+console.log("-------------------------------------------------- 2");
+
+/*Use the Spread Operator to Evaluate Arrays In-Place
+
+ES6 introduces the spread operator, which allows us to expand arrays and other expressions in places where multiple parameters or elements are expected.*/
+
+// Example
+const arr = [6, 89, 3, 45];
+const maximus = Math.max(...arr);
+console.log(maximus); // 89
+
+// Exercise
+
+const arr1 = ["JAN", "FEB", "MAR", "APR", "MAY"];
+let arr2;
+
+arr2 = [...arr1]; // Change this line
+
+console.log(arr2);
+
+// print separator line
+console.log("-------------------------------------------------- 3");

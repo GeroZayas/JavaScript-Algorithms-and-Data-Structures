8811ab7e5c4a499f9f00591eeab87abb22825cf6
GeroZayas <gerozayas@gmail.com>
2022-04-05 23:36:21 +0200

    Update

--- Strings_using_Template_Literals.js
+++ Strings_using_Template_Literals.js
@@ -1,3 +1,41 @@
 /*A new feature of ES6 is the template literal. This is a special type of string that makes creating complex strings easier.
 
 Template literals allow you to create multi-line strings and to use string interpolation features to create strings.*/
+
+const person = {
+  name: "Zodiac Hasbro",
+  age: 56,
+};
+
+const greeting = `Hello, my name is ${person.name}!
+  I am ${person.age} years old.`;
+
+console.log(greeting);
+
+/*A lot of things happened there. Firstly, the example uses backticks (`), not quotes (' or "), to wrap the string. Secondly, notice that the string is multi-line, both in the code and the output. This saves inserting \n within strings. The ${variable} syntax used above is a placeholder. Basically, you won't have to use concatenation with the + operator anymore.*/
+
+// Exercise
+console.log("------------------ Exercise ------------------");
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"],
+};
+
+function makeList(arr) {
+  // Only change code below this line
+
+  const failureItems = [];
+  for (let i = 0; i < arr.length; i++) {
+    failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+  }
+
+  // Only change code above this line
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
+
+console.log(failuresList);

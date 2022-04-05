1cf21035df63a65ff13e6db344c0feb21677c516
GeroZayas <gerozayas@gmail.com>
2022-04-05 23:40:00 +0200

    Update

--- Object_Property_Shorthand.js
+++ Object_Property_Shorthand.js
@@ -1,6 +1,14 @@
+// ES6 adds some nice support for easily defining object literals.
+
 const getMousePosition = (x, y) => ({
   x: x,
   y: y,
 });
 
-console.log(getMousePosition(1, 2));
+console.log(getMousePosition(1, 2)); // {x: 1, y: 2}
+
+/*getMousePosition is a simple function that returns an object containing two properties. ES6 provides the syntactic sugar to eliminate the redundancy of having to write x: x. You can simply write x once, and it will be converted tox: x (or something equivalent) under the hood. Here is the same function from above rewritten to use this new syntax:*/
+
+const getTheMousePosition = (x, y) => ({ x, y });
+
+console.log(getMousePosition(1, 2)); // {x: 1, y: 2}

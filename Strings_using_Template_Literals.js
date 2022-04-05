e17846fb07a9ef7d36da66e68b389523a0f68cbf
GeroZayas <gerozayas@gmail.com>
2022-04-05 23:27:20 +0200

    Update

---
+++ Strings_using_Template_Literals.js
@@ -0,0 +1,3 @@
+/*A new feature of ES6 is the template literal. This is a special type of string that makes creating complex strings easier.
+
+Template literals allow you to create multi-line strings and to use string interpolation features to create strings.*/

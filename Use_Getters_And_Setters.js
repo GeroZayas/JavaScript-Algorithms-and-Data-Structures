65ff81cd84aa0097b6f71d9a0b7a22137e15abf8
GeroZayas <gerozayas@gmail.com>
2022-04-08 12:08:02 +0200

    Update

--- Use_Getters_And_Setters.js
+++ Use_Getters_And_Setters.js
@@ -16,3 +16,23 @@ const novel = new Book("anonymous");
 console.log(novel.writer);
 novel.writer = "newAuthor";
 console.log(novel.writer);
+
+class Thermostat {
+  constructor(fahrenheit) {
+    this.fahrenheit = fahrenheit;
+  }
+  get temperature() {
+    return (5 / 9) * (this.fahrenheit - 32);
+  }
+
+  set temperature(celsius) {
+    this.fahrenheit = (celsius * 9.0) / 5 + 32;
+  }
+}
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
+
+console.log(temp); // 26 in Celsius

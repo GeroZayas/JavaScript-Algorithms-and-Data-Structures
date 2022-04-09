a70a201ec60f355d19f3896682df606f26ca5103
GeroZayas <gerozayas@gmail.com>
2022-04-09 22:01:02 +0200

    Update

--- Promise_practice.js
+++ Promise_practice.js
@@ -21,10 +21,9 @@ const myPromise = new Promise((resolve, reject) => {
 const makeServerRequest = new Promise((resolve, reject) => {
   // responseFromServer represents a response from a server
   let responseFromServer;
-
   if (responseFromServer) {
-    // Change this line
+    resolve("We got the data");
   } else {
-    // Change this line
+    reject("Data not received");
   }
 });

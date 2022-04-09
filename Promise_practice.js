87c6ef7ebcc8a57e3b0640277ec1908f727a18d6
GeroZayas <gerozayas@gmail.com>
2022-04-09 21:58:06 +0200

    Update

--- Promise_practice.js
+++ Promise_practice.js
@@ -1,6 +1,30 @@
-const myPromise = new Promise((resolve, reject) => {});
+// const myPromise = new Promise((resolve, reject) => {});
+
+// const makeServerRequest = new Promise((resolve, reject) => {
+//   // responseFromServer is set to true to represent a successful response from a server
+//   let responseFromServer = true;
+// });
+
+/*A promise has three states: pending, fulfilled, and rejected. The promise you created in the last challenge is forever stuck in the pending state because you did not add a way to complete the promise. The resolve and reject parameters given to the promise argument are used to do this. resolve is used when you want your promise to succeed, and reject is used when you want it to fail. These are methods that take an argument, as seen below.*/
+
+/*
+const myPromise = new Promise((resolve, reject) => {
+    if(condition here) {
+      resolve("Promise was fulfilled");
+    } else {
+      reject("Promise was rejected");
+    }
+  });
+
+  */
 
 const makeServerRequest = new Promise((resolve, reject) => {
-  // responseFromServer is set to true to represent a successful response from a server
-  let responseFromServer = true;
+  // responseFromServer represents a response from a server
+  let responseFromServer;
+
+  if (responseFromServer) {
+    // Change this line
+  } else {
+    // Change this line
+  }
 });

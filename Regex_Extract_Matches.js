0152ad8df67963cf7cfb54375a5a08713a4de6d5
GeroZayas <gerozayas@gmail.com>
2022-04-11 16:58:37 +0200

    Wildcard period

--- Regex_Extract_Matches.js
+++ Regex_Extract_Matches.js
@@ -40,3 +40,9 @@ let starRegex = /twinkle/gi; // notice the g for multiple extracts and i for cas
 let result2 = twinkleStar.match(starRegex);
 
 console.log(result2);
+
+// Match Anything with Wildcard Period
+
+/*
+The wildcard character . will match any one character. The wildcard is also called dot and period. You can use the wildcard character just like any other character in the regex. For example, if you wanted to match hug, huh, hut, and hum, you can use the regex /hu./ to match all four words.
+*/

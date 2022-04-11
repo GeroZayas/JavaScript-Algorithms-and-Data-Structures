4339c7334630f28339ea4767ee2ce52135377cf0
GeroZayas <gerozayas@gmail.com>
2022-04-11 16:41:32 +0200

    Update

--- Regex_Test_Method.js
+++ Regex_Test_Method.js
@@ -4,3 +4,9 @@ let myRegex = /Hello/;
 let result = myRegex.test(myString);
 
 console.log(result); // true
+
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let result2 = waldoRegex.test(waldoIsHiding);
+
+console.log(result2);

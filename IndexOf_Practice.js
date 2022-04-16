7d57a6f900b184b35c911da083efd4f9be2fe027
GeroZayas <gerozayas@gmail.com>
2022-04-17 00:14:44 +0200

    Update

--- IndexOf_Practice.js
+++ IndexOf_Practice.js
@@ -1,5 +1,4 @@
 function quickCheck(arr, elem) {
-  // Only change code below this line
   return arr.indexOf(elem) >= 0 ? true : false;
 }
 console.log(quickCheck(["squash", "onions", "shallots"], "mushrooms")); // false

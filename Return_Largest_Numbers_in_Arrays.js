c68a8be083fd46cfc0f26073297d0603ef22f571
GeroZayas <gerozayas@gmail.com>
2022-04-17 18:29:36 +0200

    Update

--- Return_Largest_Numbers_in_Arrays.js
+++ Return_Largest_Numbers_in_Arrays.js
@@ -1,15 +1,24 @@
 //Return Largest Numbers in Arrays
 
-// function largestOfFour(arr) {
-//   largestNumbers = [];
-//   for (let array = 0; array < arr.length; array++) {
-//     largestNumbers.push(Math.max(...arr[array]));
-//   }
-//   return largestNumbers;
-// }
+function largestOfFour(arr) {
+  largestNumbers = [];
+  for (let array = 0; array < arr.length; array++) {
+    largestNumbers.push(Math.max(...arr[array]));
+  }
+  return largestNumbers;
+}
+
+console.log(
+  largestOfFour([
+    [13, 27, 18, 26],
+    [4, 5, 1, 3],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1],
+  ])
+);
 
 function largestOfFour(arr) {
-  let largestNumbers = [];
+  const largestNumbers = [];
   let numHolder = 0;
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr[i].length; j++) {
@@ -17,7 +26,7 @@ function largestOfFour(arr) {
         numHolder = arr[i][j];
       }
     }
-    largestNumbers.push(numHolder);
+    largestNumbers[i] = numHolder;
   }
   return largestNumbers;
 }

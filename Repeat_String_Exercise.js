bd035dee3228a2fc04db3726f978b17f28a6f09e
GeroZayas <gerozayas@gmail.com>
2022-04-18 23:46:46 +0200

    Update

--- Repeat_String_Exercise.js
+++ Repeat_String_Exercise.js
@@ -2,7 +2,8 @@
 Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number. For the purpose of this challenge, do not use the built-in .repeat() method.*/
 
 function repeatStringNumTimes(str, num) {
-  return str;
+  let finalString = "";
+  return finalString;
 }
 
-repeatStringNumTimes("abc", 3);
+console.log(repeatStringNumTimes("abc", 3));

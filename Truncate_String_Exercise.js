61a7d214f7e6fae9e073b3555a2445e80eebc726
GeroZayas <gerozayas@gmail.com>
2022-04-18 23:55:04 +0200

    Update

--- Truncate_String_Exercise.js
+++ Truncate_String_Exercise.js
@@ -4,7 +4,17 @@ Truncate a string (first argument) if it is longer than the given maximum string
  */
 
 function truncateString(str, num) {
+  if (str.length > num) {
+    str = str.slice(0, num) + "...";
+  }
   return str;
 }
 
-truncateString("A-tisket a-tasket A green and yellow basket", 8);
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
+
+console.log(
+  truncateString(
+    "A-tisket a-tasket A green and yellow basket",
+    "A-tisket a-tasket A green and yellow basket".length
+  )
+);

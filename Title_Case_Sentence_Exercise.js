518d0d8931a6620291c5d0ee0b0e64898cc7fbb9
GeroZayas <gerozayas@gmail.com>
2022-04-20 23:34:50 +0200

    Add comments

--- Title_Case_Sentence_Exercise.js
+++ Title_Case_Sentence_Exercise.js
@@ -5,15 +5,21 @@
 For the purpose of this exercise, you should also capitalize connecting words like the and of. */
 
 function titleCase(str) {
+  // Create the string to be returned
   let finalStr = "";
+  // Split the input string to be able to work with each individual word
   let splitStr = str.split(" ");
+  // We make all the words lower case
   for (let i = 0; i < splitStr.length; i++) {
     splitStr[i] = splitStr[i].toLowerCase();
   }
+  // We select the first letter of each word -> .charAt() and capitalize it
+  // -> toUpperCase()
   for (let i = 0; i < splitStr.length; i++) {
     finalStr +=
       splitStr[i].charAt(0).toUpperCase() + splitStr[i].slice(1) + " ";
   }
+  // NOTICE the .trim() -> to eliminate space at the end of string
   return finalStr.trim();
 }
 

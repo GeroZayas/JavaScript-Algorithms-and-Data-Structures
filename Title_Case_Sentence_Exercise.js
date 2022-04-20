2ca395b123d42cc356f7c98ee00f57fba74c90af
GeroZayas <gerozayas@gmail.com>
2022-04-20 23:25:15 +0200

    Update

--- Title_Case_Sentence_Exercise.js
+++ Title_Case_Sentence_Exercise.js
@@ -1,1 +1,16 @@
 // Title Case a Sentence
+
+/* Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+
+For the purpose of this exercise, you should also capitalize connecting words like the and of. */
+
+function titleCase(str) {
+  let finalStr = "";
+  let splitStr = str.split(" ");
+  for (let i = 0; i < splitStr.length; i++) {
+    finalStr += splitStr[i].toUpperCase() + " ";
+  }
+  return finalStr;
+}
+
+console.log(titleCase("I'm a little tea pot"));

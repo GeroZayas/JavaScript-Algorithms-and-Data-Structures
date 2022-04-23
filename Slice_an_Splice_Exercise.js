046b0a20ad7aed0c6bbaf970a08fa64568854c12
GeroZayas <gerozayas@gmail.com>
2022-04-23 10:52:41 +0200

    Add comments

--- Slice_an_Splice_Exercise.js
+++ Slice_an_Splice_Exercise.js
@@ -11,7 +11,11 @@ Return the resulting array. The input arrays should remain the same after the fu
 
 */
 function frankenSplice(arr1, arr2, n) {
+  // Since our goal is to return the new array with out altering arr1 or arr2
+  // we create a localArr and add all the items from arr2 using the slice() function
   let localArr = arr2.slice();
+  // here we use splice and n is the index, 0 is for no elements being cut out in this case
+  // and then we ...arr1 to insert arr1
   localArr.splice(n, 0, ...arr1);
   return localArr;
 }

66c511d24d4ecc0a4c3ae40bfd9f291e4c599d1a
GeroZayas <gerozayas@gmail.com>
2022-04-25 23:06:31 +0200

    Add comments

--- Falsy_Bouncer.js
+++ Falsy_Bouncer.js
@@ -9,11 +9,15 @@ Hint: Try converting each value to a Boolean.
 */
 
 function bouncer(arr) {
+  // Declare he array to be returned
   let newArray = [];
+  // We create a for loop to go over each element of arr (input)
   for (let i = 0; i < arr.length; i++) {
+    // we create an if to check if its of type of boolean
+    // we add to newArray if it is boolean
     if (arr[i]) newArray.push(arr[i]);
   }
   return newArray;
 }
 
-console.log(bouncer([7, "ate", "", false, 9]));
+console.log(bouncer([7, "ate", "", false, 9])); // [ 7, 'ate', 9 ]

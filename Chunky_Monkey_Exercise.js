26054964abe8ed621d4f6b26c06614299ba1b766
GeroZayas <gerozayas@gmail.com>
2022-04-29 19:26:37 +0200

    Add comment

--- Chunky_Monkey_Exercise.js
+++ Chunky_Monkey_Exercise.js
@@ -6,7 +6,11 @@ function chunkArrayInGroups(arr, size) {
   let result = [];
 
   for (let a = 0; a < arr.length; a++) {
+    // Inside our loop, we push to temp using temp.push(arr[a]);
+    // if the remainder of a / size is not equal to size - 1.
     if (a % size !== size - 1) temp.push(arr[a]);
+    // Otherwise, we push to temp, push temp to the result variable
+    // and reset temp to an empty array.
     else {
       temp.push(arr[a]);
       result.push(temp);

ce78c111088aae64df5d5f029756bf4ec9737b3c
GeroZayas <gerozayas@gmail.com>
2022-05-01 18:04:20 +0200

    Initial

---
+++ Create_Object_Basic_Exercise.js
@@ -0,0 +1,14 @@
+//Very basic exercise
+// Create an object
+
+// We create an object dog
+
+let dog = {
+  name: "Yuni",
+  numLegs: 4,
+};
+
+console.log("The name of the dog is ");
+console.log(dog.name);
+console.log("The number of legs is ");
+console.log(dog.numLegs);

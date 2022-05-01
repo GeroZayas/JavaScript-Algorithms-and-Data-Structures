5db25f8449fc751b4032f3ed24e09b368c97c377
GeroZayas <gerozayas@gmail.com>
2022-05-01 18:06:55 +0200

    add function

--- Create_Object_Basic_Exercise.js
+++ Create_Object_Basic_Exercise.js
@@ -6,9 +6,13 @@
 let dog = {
   name: "Yuni",
   numLegs: 4,
+  sayLegs: function () {
+    return "This dog has " + dog.numLegs + " legs.";
+  },
 };
 
 console.log("The name of the dog is ");
 console.log(dog.name);
 console.log("The number of legs is ");
 console.log(dog.numLegs);
+console.log();

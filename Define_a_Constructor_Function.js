51fdf09e6069d17da5fec6d7656169e6f4177081
GeroZayas <gerozayas@gmail.com>
2022-05-01 18:11:41 +0200

    Update

--- Define_a_Constructor_Function.js
+++ Define_a_Constructor_Function.js
@@ -1,1 +1,21 @@
 // Define a Constructor Function
+
+/*Constructors are functions that create new objects. They define properties and behaviors that will belong to the new object. Think of them as a blueprint for the creation of new objects.
+
+Here is an example of a constructor:
+
+function Bird() {
+  this.name = "Albert";
+  this.color = "blue";
+  this.numLegs = 2;
+}*/
+
+/*Create a constructor, Dog, with properties name, color, and numLegs that are set to a string, a string, and a number, respectively.*/
+
+function Dog() {
+  this.name = "Yuni";
+  this.color = "Black and White";
+  this.numLegs = 4;
+}
+
+console.log();

51696100bd024347823560abd83c4f6a45002fec
GeroZayas <gerozayas@gmail.com>
2022-05-07 18:04:37 +0200

    Update

--- Change_the_Prototype_to_a_New_Object.js
+++ Change_the_Prototype_to_a_New_Object.js
@@ -26,11 +26,16 @@ Bird.prototype = {
 
 // Exercise
 /*Add the property numLegs and the two methods eat() and describe() to the prototype of Dog by setting the prototype to a new object.*/
-
 function Dog(name) {
   this.name = name;
 }
 
 Dog.prototype = {
-  // Only change code below this line
+  numLegs: 2,
+  eat: function () {
+    console.log("yum yum yum");
+  },
+  describe: function () {
+    console.log(this.name);
+  },
 };

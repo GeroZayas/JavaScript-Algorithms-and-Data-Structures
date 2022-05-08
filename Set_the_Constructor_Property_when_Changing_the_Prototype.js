3631c011625ecec9d39715dd7d3c489eb9870b44
GeroZayas <gerozayas@gmail.com>
2022-05-08 23:41:24 +0200

    Add exercise

--- Set_the_Constructor_Property_when_Changing_the_Prototype.js
+++ Set_the_Constructor_Property_when_Changing_the_Prototype.js
@@ -23,3 +23,18 @@ Bird.prototype = {
 
 // Exercise
 // Define the constructor property on the Dog prototype.
+
+function Dog(name) {
+  this.name = name;
+}
+
+// Only change code below this line
+Dog.prototype = {
+  numLegs: 4,
+  eat: function () {
+    console.log("nom nom nom");
+  },
+  describe: function () {
+    console.log("My name is " + this.name);
+  },
+};

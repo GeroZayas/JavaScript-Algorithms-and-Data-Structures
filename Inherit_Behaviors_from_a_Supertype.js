3e0d5ec8583238f3e1a93c0ea9f01e71cd521dbb
GeroZayas <gerozayas@gmail.com>
2022-05-16 22:47:34 +0200

    Add exercise

--- Inherit_Behaviors_from_a_Supertype.js
+++ Inherit_Behaviors_from_a_Supertype.js
@@ -18,3 +18,7 @@ Object.create(obj) creates a new object, and sets obj as the new object's protot
 animal.eat();
 animal instanceof Animal;
 The instanceof method here would return true.*/
+
+// Exercise
+
+/*Use Object.create to make two instances of Animal named duck and beagle.*/

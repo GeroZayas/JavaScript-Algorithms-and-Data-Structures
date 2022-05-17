4542332c031b3cee95a1a7b275f7e6de760ff10b
GeroZayas <gerozayas@gmail.com>
2022-05-17 23:56:52 +0200

    Add solution

--- Set_the_Child's_Prototype_to_an_Instance_of_the_Parent.js
+++ Set_the_Child's_Prototype_to_an_Instance_of_the_Parent.js
@@ -16,3 +16,21 @@ duck inherits all of Animal's properties, including the eat method.
 // Exercise
 
 /*Modify the code so that instances of Dog inherit from Animal.*/
+
+function Animal() {}
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function () {
+    console.log("nom nom nom");
+  },
+};
+
+function Dog() {}
+
+// Only change code below this line
+
+//solution
+Dog.prototype = Object.create(Animal.prototype);
+
+let beagle = new Dog();

e3c80816de8be3df3bc02c161dd5e24a736e97e4
GeroZayas <gerozayas@gmail.com>
2022-05-18 23:49:49 +0200

    Add exercise

--- Reset_an_Inherited_Constructor_Property.js
+++ Reset_an_Inherited_Constructor_Property.js
@@ -17,3 +17,15 @@ duck.constructor
 // Exercise
 
 //Fix the code so duck.constructor and beagle.constructor return their respective constructors.
+
+function Animal() {}
+function Bird() {}
+function Dog() {}
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+// Only change code below this line
+
+let duck = new Bird();
+let beagle = new Dog();

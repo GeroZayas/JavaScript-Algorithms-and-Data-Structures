a9bd3b440c5a50bdf250e0354117dc671a43ce39
GeroZayas <gerozayas@gmail.com>
2022-05-19 23:53:26 +0200

    Update exercise

--- Add_Methods_After_Inheritance.js
+++ Add_Methods_After_Inheritance.js
@@ -28,3 +28,16 @@ duck.eat() would display the string nom nom nom in the console, and duck.fly() w
 // Exercise
 
 /*Add all necessary code so the Dog object inherits from Animal and the Dog's prototype constructor is set to Dog. Then add a bark() method to the Dog object so that beagle can both eat() and bark(). The bark() method should print Woof! to the console.*/
+
+function Animal() {}
+Animal.prototype.eat = function () {
+  console.log("nom nom nom");
+};
+
+function Dog() {}
+
+// Only change code below this line
+
+// Only change code above this line
+
+let beagle = new Dog();

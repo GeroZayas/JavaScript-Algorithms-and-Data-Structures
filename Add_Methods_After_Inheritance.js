42a69c3ef697f0492bcc75728e437cd52df9cb6d
GeroZayas <gerozayas@gmail.com>
2022-05-19 23:54:55 +0200

    Add solution

--- Add_Methods_After_Inheritance.js
+++ Add_Methods_After_Inheritance.js
@@ -37,7 +37,11 @@ Animal.prototype.eat = function () {
 function Dog() {}
 
 // Only change code below this line
-
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+Dog.prototype.bark = function () {
+  console.log("Woof!");
+};
 // Only change code above this line
 
 let beagle = new Dog();

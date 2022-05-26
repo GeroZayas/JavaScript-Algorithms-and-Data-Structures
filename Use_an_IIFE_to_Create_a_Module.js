407f0f66adfdb8d672574c22bce4e325d38781ff
GeroZayas <gerozayas@gmail.com>
2022-05-26 22:50:41 +0200

    add exercise

--- Use_an_IIFE_to_Create_a_Module.js
+++ Use_an_IIFE_to_Create_a_Module.js
@@ -36,3 +36,14 @@ duck.glide();*/
 // Exercise
 
 /*Create a module named funModule to wrap the two mixins isCuteMixin and singMixin. funModule should return an object.*/
+
+let isCuteMixin = function (obj) {
+  obj.isCute = function () {
+    return true;
+  };
+};
+let singMixin = function (obj) {
+  obj.sing = function () {
+    console.log("Singing to an awesome tune");
+  };
+};

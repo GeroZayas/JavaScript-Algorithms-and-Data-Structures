8215eec6db316927cae60a26ed728887edaeee9a
GeroZayas <gerozayas@gmail.com>
2022-05-28 12:48:00 +0200

    Add exercise

--- Functional_Programming.js
+++ Functional_Programming.js
@@ -10,3 +10,10 @@ Isolated functions - there is no dependence on the state of the program, which i
 Pure functions - the same input always gives the same output
 
 Functions with limited side effects - any changes, or mutations, to the state of the program outside the function are carefully controlled*/
+
+// Exercise
+
+/*The members of freeCodeCamp happen to love tea.
+
+In the code editor, the prepareTea and getTea functions are already defined for you. Call the getTea function to get 40 cups of tea for the team, and store them in the tea4TeamFCC variable.
+*/

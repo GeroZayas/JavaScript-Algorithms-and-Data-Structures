c631ebae84d5fa8d93bd5446446f8346dfdfdcdd
GeroZayas <gerozayas@gmail.com>
2022-06-08 23:55:41 +0200

    update

--- Avoid_Mutations_and_Side_Effects_Using_Functional_Programming.js
+++ Avoid_Mutations_and_Side_Effects_Using_Functional_Programming.js
@@ -4,6 +4,9 @@
 // FINISH THIS ONE
 // FINISH THIS ONE
 
+// FINISH THIS ONE
+// FINISH THIS ONE
+// FINISH THIS ONE
 
 /*If you haven't already figured it out, the issue in the previous challenge was with the splice call in the tabClose() function. Unfortunately, splice changes the original array it is called on, so the second call to it used a modified array, and gave unexpected results.
 

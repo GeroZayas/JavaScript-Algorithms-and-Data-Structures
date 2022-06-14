6e0d81de86d2045a5101e4dd07852add2e587922
GeroZayas <gerozayas@gmail.com>
2022-06-14 23:32:26 +0200

    Update exercise

--- Pass_Arguments_Avoid_External_Dependence_in_a_Function.js
+++ Pass_Arguments_Avoid_External_Dependence_in_a_Function.js
@@ -19,3 +19,11 @@ Finally, the function would always produce the same output for the same set of i
 Write the incrementer function so it takes an argument, and then returns a result after increasing the value by one.
 
 */
+
+// The global variable
+let fixedValue = 4;
+
+// Only change code below this line
+function incrementer() {
+  // Only change code above this line
+}

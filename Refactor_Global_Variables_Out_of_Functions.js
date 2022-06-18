767e3d708edde75083a29644175d2a1d8874b907
GeroZayas <gerozayas@gmail.com>
2022-06-18 12:13:13 +0200

    Add exercise

--- Refactor_Global_Variables_Out_of_Functions.js
+++ Refactor_Global_Variables_Out_of_Functions.js
@@ -7,3 +7,35 @@ Don't alter a variable or object - create new variables and objects and return t
 Declare function parameters - any computation inside a function depends only on the arguments passed to the function, and not on any global object or variable.
 
 Adding one to a number is not very exciting, but we can apply these principles when working with arrays or more complex objects.*/
+
+// Exercise
+
+/*Rewrite the code so the global array bookList is not changed inside either function. The add function should add the given bookName to the end of the array passed to it and return a new array (list). The remove function should remove the given bookName from the array passed to it.
+
+Note: Both functions should return an array, and any new parameters should be added before the bookName parameter.*/
+// The global variable
+const bookList = [
+  "The Hound of the Baskervilles",
+  "On The Electrodynamics of Moving Bodies",
+  "Philosophiæ Naturalis Principia Mathematica",
+  "Disquisitiones Arithmeticae",
+];
+
+// Change code below this line
+function add(bookName) {
+  bookList.push(bookName);
+  return bookList;
+
+  // Change code above this line
+}
+
+// Change code below this line
+function remove(bookName) {
+  const book_index = bookList.indexOf(bookName);
+  if (book_index >= 0) {
+    bookList.splice(book_index, 1);
+    return bookList;
+
+    // Change code above this line
+  }
+}

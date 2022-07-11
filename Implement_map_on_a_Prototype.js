00541bea7c47456877940cf046d9f6c801c2b2ef
GeroZayas <gerozayas@gmail.com>
2022-07-11 11:43:39 +0200

    Upate

--- Implement_map_on_a_Prototype.js
+++ Implement_map_on_a_Prototype.js
@@ -1,4 +1,12 @@
 /*Implement map on a Prototype
+
+// COMPLETE THIS ONE
+// COMPLETE THIS ONE
+// COMPLETE THIS ONE
+// COMPLETE THIS ONE
+// COMPLETE THIS ONE
+// COMPLETE THIS one
+
 As you have seen from applying Array.prototype.map(), or simply map() earlier, the map method returns an array of the same length as the one it was called on. It also doesn't alter the original array, as long as its callback function doesn't.
 
 In other words, map is a pure function, and its output depends solely on its inputs. Plus, it takes another function as its argument.

c26aa1181ed993774c5295c421c73bf85d23fff3
GeroZayas <gerozayas@gmail.com>
2022-07-11 11:42:56 +0200

    A exercise

--- Implement_map_on_a_Prototype.js
+++ Implement_map_on_a_Prototype.js
@@ -6,3 +6,4 @@ In other words, map is a pure function, and its output depends solely on its inp
 You might learn a lot about the map method if you implement your own version of it. It is recommended you use a for loop or Array.prototype.forEach().*/
 
 // Exercise
+/*Write your own Array.prototype.myMap(), which should behave exactly like Array.prototype.map(). You should not use the built-in map method. The Array instance can be accessed in the myMap method using this.*/

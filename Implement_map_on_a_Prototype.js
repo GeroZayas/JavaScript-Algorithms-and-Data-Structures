e1f5931415d0604158cc6df847a4ff5ad607d2f3
GeroZayas <gerozayas@gmail.com>
2022-07-11 11:43:17 +0200

    Add exercise

--- Implement_map_on_a_Prototype.js
+++ Implement_map_on_a_Prototype.js
@@ -7,3 +7,18 @@ You might learn a lot about the map method if you implement your own version of 
 
 // Exercise
 /*Write your own Array.prototype.myMap(), which should behave exactly like Array.prototype.map(). You should not use the built-in map method. The Array instance can be accessed in the myMap method using this.*/
+
+// The global variable
+const s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function (callback) {
+  const newArray = [];
+  // Only change code below this line
+
+  // Only change code above this line
+  return newArray;
+};
+
+const new_s = s.myMap(function (item) {
+  return item * 2;
+});

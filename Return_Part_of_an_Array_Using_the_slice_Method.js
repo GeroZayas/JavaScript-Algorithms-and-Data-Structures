0f81dbde4568a69fd704127bb50750d0f7442fa0
GeroZayas <gerozayas@gmail.com>
2022-09-02 18:41:41 +0200

    add exercise

--- Return_Part_of_an_Array_Using_the_slice_Method.js
+++ Return_Part_of_an_Array_Using_the_slice_Method.js
@@ -12,3 +12,11 @@ newArray would have the value ["Dog", "Tiger"].*/
 // Exercise
 
 /*Use the slice method in the sliceArray function to return part of the anim array given the provided beginSlice and endSlice indices. The function should return an array.*/
+
+function sliceArray(anim, beginSlice, endSlice) {
+  // Only change code below this line
+  // Only change code above this line
+}
+
+const inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+sliceArray(inputAnim, 1, 3);

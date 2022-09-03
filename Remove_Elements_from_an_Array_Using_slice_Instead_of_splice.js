97c973bf60b160622d4705add33cd22535a76b6a
GeroZayas <gerozayas@gmail.com>
2022-09-03 11:16:34 +0200

    Add solution

--- Remove_Elements_from_an_Array_Using_slice_Instead_of_splice.js
+++ Remove_Elements_from_an_Array_Using_slice_Instead_of_splice.js
@@ -16,10 +16,10 @@ Do not mutate the original array provided to the function.*/
 
 function nonMutatingSplice(cities) {
   // Only change code below this line
-  return cities.splice(3);
+  return cities.slice(0, 3);
 
   // Only change code above this line
 }
 
 const inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
-nonMutatingSplice(inputCities);
+console.log(nonMutatingSplice(inputCities)); // [ 'Chicago', 'Delhi', 'Islamabad' ]

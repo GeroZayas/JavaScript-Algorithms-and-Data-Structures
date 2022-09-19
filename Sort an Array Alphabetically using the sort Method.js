c8c35329a647881aee6a79af14c4848684b46ef7
GeroZayas <gerozayas@gmail.com>
2022-09-19 23:56:19 +0200

    add exercise

--- Sort an Array Alphabetically using the sort Method.js
+++ Sort an Array Alphabetically using the sort Method.js
@@ -27,3 +27,12 @@ JavaScript's default sorting method is by string Unicode point value, which may 
 // EXERCISE
 
 /*Use the sort method in the alphabeticalOrder function to sort the elements of arr in alphabetical order. The function should return the sorted array.*/
+
+function alphabeticalOrder(arr) {
+  // Only change code below this line
+
+  return arr.sort();
+  // Only change code above this line
+}
+
+alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);

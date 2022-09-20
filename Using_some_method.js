c2e7e05807fac8ed359666d3fe38a3c6b6bb6e08
GeroZayas <gerozayas@gmail.com>
2022-09-20 07:46:49 +0200

    update

--- Using_some_method.js
+++ Using_some_method.js
@@ -1,1 +1,13 @@
 // Using the some method
+
+//For example, the following code would check if any element in the numbers array is less than 10:
+
+const numbers = [10, 50, 8, 220, 110, 11];
+
+let answer = numbers.some(function (currentValue) {
+  return currentValue < 10;
+});
+
+// The some method would return true.
+
+console.log(answer); // true

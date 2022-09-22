743cba0062285e6e40ce4a870675c69e7fdfc1b5
GeroZayas <gerozayas@gmail.com>
2022-09-22 08:34:19 +0200

    add solution

--- Use_of_arguments_object.js
+++ Use_of_arguments_object.js
@@ -5,6 +5,19 @@ function destroyer(arr) {
   // we create the arr we want to return
   const filteredArray = [];
 
+  for (let i = 0; i < arr.length; i++) {
+    let removeElement = false;
+    for (let j = 0; j < valsToRemove.length; j++) {
+      if (arr[i] === valsToRemove[j]) {
+        removeElement = true;
+      }
+    }
+
+    if (!removeElement) {
+      filteredArray.push(arr[i]);
+    }
+  }
+
   return filteredArray;
 }
 

1197ae0c1094c30d28a550356619b7e2e88c84ae
GeroZayas <gerozayas@gmail.com>
2022-09-22 08:30:58 +0200

    update

--- Use_of_arguments_object.js
+++ Use_of_arguments_object.js
@@ -1,8 +1,11 @@
 // Use of *arguments* object
 function destroyer(arr) {
-  //
+  //this slice here makes an array with the rest of the values, after the first one, which is the one we want to keep
   const valsToRemove = Object.values(arguments).slice(1);
-  return valsToRemove;
+  // we create the arr we want to return
+  const filteredArray = [];
+
+  return filteredArray;
 }
 
 console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));

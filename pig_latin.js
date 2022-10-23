c474b5aba7b0ced1cc358725898deac763d61bbc
GeroZayas <gerozayas@gmail.com>
2022-10-24 00:50:37 +0200

    intial

---
+++ pig_latin.js
@@ -0,0 +1,9 @@
+//  - If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add ay to it.
+
+// - If a word begins with a vowel, just add way at the end.
+
+function translatePigLatin(str) {
+  return str;
+}
+
+translatePigLatin("consonant");

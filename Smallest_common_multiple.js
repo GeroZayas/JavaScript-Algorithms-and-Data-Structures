6df1598d1777be83fb0520f348e3a5a819d373a5
GeroZayas <gerozayas@gmail.com>
2022-10-25 23:48:50 +0200

    add slution

--- Smallest_common_multiple.js
+++ Smallest_common_multiple.js
@@ -1,7 +1,23 @@
-// Smallest Common Multiple
-
 function smallestCommons(arr) {
-  return arr;
+  // declare 2 vars called max and min that have the value of Math.max operations
+  var max = Math.max(arr[0], arr[1]);
+  var min = Math.min(arr[0], arr[1]);
+
+  // The reason to have something declared twice like this is for functions where one of this will be changing as the function goes on:
+  var mltple = max;
+
+  // The value of i, which starts on max will decrease by one (i--) until is bigger or equal to min (i>=min)
+  for (var i = max; i >= min; i--) {
+    // If the remainder of the division is different to 0, add max to mltple
+    // mltple += max is the same as mltple = mltple + max
+    if (mltple % i !== 0) {
+      mltple += max;
+      i = max;
+    }
+  }
+
+  // when the condition i >= min is met, return the mltple
+  return mltple;
 }
 
-smallestCommons([1, 5]);
+console.log(smallestCommons([1, 5]));

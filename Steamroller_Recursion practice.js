8a02fad0a94facee7b01062b7bd186fce832dafe
GeroZayas <gerozayas@gmail.com>
2022-10-27 09:48:58 +0200

    update comments

--- Steamroller_Recursion practice.js
+++ Steamroller_Recursion practice.js
@@ -1,6 +1,9 @@
 /*Steamroller
 Flatten a nested array. You must account for varying levels of nesting.*/
 
+// using recursion, .reduce() and .concat() methods
+// works with arrays of any depth
+
 function steamrollArray(arr) {
   return arr.reduce(
     (acc, cur) => acc.concat(Array.isArray(cur) ? steamrollArray(cur) : cur),
